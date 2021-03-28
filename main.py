96a968e84976c31350e1920fa00e412ec65b4562
Icode-python <me.fast.learn@gmail.com>
2021-03-27 21:03:57 -0400

    started coding

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 import pygame
+import numpy as np
+from settings import *
+from entities import *
+import threading
+
+class Game:
+    
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption(TITLE)
+        self.clock = pygame.time.Clock()
+        self.enemies = []
+        self.bullets = []
+        self.load_data()
+
+    def load_data(self):
+        pass
+
+    def new(self):
+        self.all_sprites = pygame.sprite.Group()
