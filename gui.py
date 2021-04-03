fea3f1a321916741ed76c2f30d70ffea6205a604
Icode-python <me.fast.learn@gmail.com>
2021-04-03 14:26:35 -0400

    finished adding start menu

--- gui.py
+++ gui.py
@@ -1,23 +1,27 @@
 import tkinter as tk
 from settings import *
 
-global start
-start = False
+class menuSystem:
+    
+    def __init__(self):
+        self.start = False
+        self.root = 0
 
-def game_start():
-    root = tk.Tk()
-    root.geometry("{}x{}".format(WIDTH,HEIGHT))
-    startbutton = tk.Button(root, text="START", bg='gray', fg='black', bd='10', padx=str(100), pady=str(50))
-    #p = startbutton.invoke()
-    startbutton.grid(row=5, column=0,padx=230,pady=HEIGHT/2-100)
-    #startbutton.pack()
-    quitbutton = tk.Button(root, text="QUIT", bg='gray', fg='black', bd='10', padx=str(100), pady=str(50), command=root.destroy)
-    quitbutton.grid(row=5, column=1, padx=0,pady=HEIGHT/2-100)
-    #quitbutton.pack()
-    root.mainloop()
+    def game_start(self):
+        self.root = tk.Tk()
+        self.root.geometry("{}x{}".format(WIDTH,HEIGHT))
+        startbutton = tk.Button(self.root, text="START", bg='gray', fg='black', bd='10', padx=str(100), pady=str(50), command=self.startgame)
+        startbutton.pack()
+        startbutton.grid(row=0, column=0,padx=230,pady=HEIGHT/2-100)
+        #startbutton.pack()
+        quitbutton = tk.Button(self.root, text="QUIT", bg='gray', fg='black', bd='10', padx=str(100), pady=str(50), command=self.root.destroy)
+        quitbutton.grid(row=0, column=1, padx=0,pady=HEIGHT/2-100)
+        #quitbutton.pack()
+        self.root.mainloop()
 
-def startgame(bool):
-    if bool:
-        return True
-    else:
-        return False
+    def startgame(self):
+        self.start = True
+        self.root.destroy()
+        if self.start == True:
+            self.root.destroy()
+            print('e')
--- main.py
+++ main.py
@@ -10,92 +10,95 @@ from gui import *
 import sys
 
 
-game_start()
+menu = menuSystem()
+print(menu.start)
+menu.game_start()
+print(menu.start)
+if menu.start == True:
+    pygame.init()
+    pygame.mixer.init()
+    sc = pygame.display.set_mode((WIDTH, HEIGHT))
+    sc_map = pygame.Surface((WIDTH // MAP_SCALE, HEIGHT // MAP_SCALE))
+    pygame.display.set_caption('AMERICA RIP OFF XD')
+    clock = pygame.time.Clock()
+    #player = Player()
+    ls = levelSystem()
+    ls.generateLevel()
+    player = ls.player
 
-pygame.init()
-pygame.mixer.init()
-sc = pygame.display.set_mode((WIDTH, HEIGHT))
-sc_map = pygame.Surface((WIDTH // MAP_SCALE, HEIGHT // MAP_SCALE))
-pygame.display.set_caption('AMERICA RIP OFF XD')
-clock = pygame.time.Clock()
-#player = Player()
-ls = levelSystem()
-ls.generateLevel()
-player = ls.player
-
-sprites = Sprites()
-drawing = Drawing(sc, sc_map)
-cursor = pygame.Surface((10,10))
-paused = False
-pygame.mouse.set_visible(paused)
-MAINTHEME.play(-1)
-MAINTHEME.set_volume(0.1)
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
+    sprites = Sprites()
+    drawing = Drawing(sc, sc_map)
+    cursor = pygame.Surface((10,10))
+    paused = False
+    pygame.mouse.set_visible(paused)
+    MAINTHEME.play(-1)
+    MAINTHEME.set_volume(0.1)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 exit()
-            if event.key == pygame.K_SPACE and paused == False:
-                if len(bulletlist) < 2 and player.ammo > 0:
-                    GUNSOUND.play()
-                    bulletlist.append(bullet(player.x, player.y, player_speed * player.cos_a, player_speed * player.sin_a))
-                    player.ammo -= 1
-            if event.key == pygame.K_TAB:
-                paused = not paused
-                pygame.mouse.set_visible(paused)
-                if paused == True:
-                    pygame.mixer.pause()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    exit()
+                if event.key == pygame.K_SPACE and paused == False:
+                    if len(bulletlist) < 2 and player.ammo > 0:
+                        GUNSOUND.play()
+                        bulletlist.append(bullet(player.x, player.y, player_speed * player.cos_a, player_speed * player.sin_a))
+                        player.ammo -= 1
+                if event.key == pygame.K_TAB:
+                    paused = not paused
+                    pygame.mouse.set_visible(paused)
+                    if paused == True:
+                        pygame.mixer.pause()
+                    else:
+                        pygame.mixer.unpause()
+            if event.type == pygame.MOUSEBUTTONDOWN and paused == False:
+                if event.button == 1:
+                    if len(bulletlist) < 2 and player.ammo > 0:
+                        GUNSOUND.play()
+                        bulletlist.append(bullet(player.x, player.y, player_speed * player.cos_a, player_speed * player.sin_a))
+                        player.ammo -= 1
+        if paused == False:
+            player.movement()
+            sc.fill(BLACK)
+            #drawing.background()
+            for mob in mobs:
+                mob.move(player.x, player.y, player)
+                mob.collisionPlayer(player)
+            all_sprites.update()
+
+            drawing.background(player.angle)
+            walls = ray_casting_walls(player, drawing.textures, ls.world_map, ls.width, ls.height)
+            drawing.world(walls + [obj.object_locate(player, walls) for obj in sprites.list_of_objects])
+            drawing.world(obj.object_locate(player, walls) for obj in list_of_objects)
+            drawing.player(player)
+            #drawing.world(walls + [mob.sprite.object_locate(player, walls) for mob in mobs])
+            drawing.fps(clock)
+            #drawing.mini_map(player)
+            if len(mobs) == 0:
+                #for sprites in all_sprites:
+                #    sprites.kill()
+                ls.levelNumber += 1
+                if ls.levelNumber < len(ls.levels):
+                    list_of_objects.clear()
+                    sprites.list_of_objects.clear() 
+                    ls.generateLevel()
+                    player = ls.player
                 else:
-                    pygame.mixer.unpause()
-        if event.type == pygame.MOUSEBUTTONDOWN and paused == False:
-            if event.button == 1:
-                if len(bulletlist) < 2 and player.ammo > 0:
-                    GUNSOUND.play()
-                    bulletlist.append(bullet(player.x, player.y, player_speed * player.cos_a, player_speed * player.sin_a))
-                    player.ammo -= 1
-    if paused == False:
-        player.movement()
-        sc.fill(BLACK)
-        #drawing.background()
-        for mob in mobs:
-            mob.move(player.x, player.y, player)
-            mob.collisionPlayer(player)
-        all_sprites.update()
+                    pygame.quit()
+                    sys.exit()
+            #drawing.drawMob()
+                #enemyRay_casting(sc, player.pos, player.angle)
+            #wwray_casting(sc, player.pos, player.angle)
+            #for wall in Walls:
+            #    sc.blit(wall.image, wall.rect)
+            #for mob in mobs:
+            #    sc.blit(mob.image, mob.rect)
+            #for b in bullets:
+            #    sc.blit(b.image, b.rect)
+            #sc.blit(player.image, player.rect)
+            sc.blit(cursor, (HALF_WIDTH, HALF_HEIGHT))
 
-        drawing.background(player.angle)
-        walls = ray_casting_walls(player, drawing.textures, ls.world_map, ls.width, ls.height)
-        drawing.world(walls + [obj.object_locate(player, walls) for obj in sprites.list_of_objects])
-        drawing.world(obj.object_locate(player, walls) for obj in list_of_objects)
-        drawing.player(player)
-        #drawing.world(walls + [mob.sprite.object_locate(player, walls) for mob in mobs])
-        drawing.fps(clock)
-        #drawing.mini_map(player)
-        if len(mobs) == 0:
-            #for sprites in all_sprites:
-            #    sprites.kill()
-            ls.levelNumber += 1
-            if ls.levelNumber < len(ls.levels):
-                list_of_objects.clear()
-                sprites.list_of_objects.clear() 
-                ls.generateLevel()
-                player = ls.player
-            else:
-                pygame.quit()
-                sys.exit()
-        #drawing.drawMob()
-            #enemyRay_casting(sc, player.pos, player.angle)
-        #wwray_casting(sc, player.pos, player.angle)
-        #for wall in Walls:
-        #    sc.blit(wall.image, wall.rect)
-        #for mob in mobs:
-        #    sc.blit(mob.image, mob.rect)
-        #for b in bullets:
-        #    sc.blit(b.image, b.rect)
-        #sc.blit(player.image, player.rect)
-        sc.blit(cursor, (HALF_WIDTH, HALF_HEIGHT))
-
-    pygame.display.flip()
-    clock.tick(FPS)
-    # print(clock.get_fps())
+        pygame.display.flip()
+        clock.tick(FPS)
+        # print(clock.get_fps())
